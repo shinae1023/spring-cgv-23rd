e5a457e0e09f3c43fdd8156a81b45bdf6c7a49de
shinae1023 <shinae1023@naver.com>
2026-06-28 12:17:55 +0900

    [Feat] cors 허용 주소 추가

--- src/main/java/com/ceos/voteservice/global/security/SecurityConfig.java
+++ src/main/java/com/ceos/voteservice/global/security/SecurityConfig.java
@@ -61,7 +61,9 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "http://localhost:8080"
+                "http://localhost:8080",
+                "https://ceos-week7.vercel.app",
+                "https://ceos-jobdri.store"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
